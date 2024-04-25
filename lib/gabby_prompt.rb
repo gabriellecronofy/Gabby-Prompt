0872aa5213678a4b764d480a6d364467ae4f7d5c
gabriellecronofy <140393993+gabriellecronofy@users.noreply.github.com>
2024-04-25 17:14:40 +0100

    Add pathname method

--- lib/gabby_prompt.rb
+++ lib/gabby_prompt.rb
@@ -2,4 +2,12 @@ class GabbyPrompt
   def arrow
    ">" 
   end
+  
+  def prompt
+    arrow
+  end
+
+  def directory_position
+    Dir.pwd
+  end
 end
--- spec/prompt_spec.rb
+++ spec/prompt_spec.rb
@@ -1,9 +1,22 @@
 require_relative "../lib/gabby_prompt"
+require "pathname"
+require "tmpdir"
 
 describe GabbyPrompt do 
   it "has a nice arrow" do
     expect(subject.arrow).to end_with(">")
   end
-  it "shows you what directory you are in"
+  it "shows you what directory you are in" do
+     Dir.mktmpdir do | dir | 
+       Dir.chdir(dir) do 
+         realpath = Pathname.new(dir).realpath.to_s
+         expect(subject.directory_position).to eq(realpath)
+       end
+    end
+    
+  end
   it "shows you the git branch"
+  it "renders the whole prompt" do
+    expect(subject.prompt).to end_with(">")
+  end
 end
