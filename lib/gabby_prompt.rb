137432410388607e397f36a4c2928cd300df2886
gabriellecronofy <140393993+gabriellecronofy@users.noreply.github.com>
2024-05-07 09:32:30 +0100

    Add github branch

--- lib/gabby_prompt.rb
+++ lib/gabby_prompt.rb
@@ -4,10 +4,18 @@ class GabbyPrompt
   end
   
   def prompt
+    array_of_git_and_dir = [ ]
+    array_of_git_and_dir << directory_position
+    array_of_git_and_dir << git_branch 
+    array_of_git_and_dir 
     arrow
   end
-
   def directory_position
     Dir.pwd
+  end
+
+  def git_branch
+     branch = `git rev-parse --abbrev-ref HEAD`.chomp
+    branch
   end
 end
--- spec/prompt_spec.rb
+++ spec/prompt_spec.rb
@@ -6,6 +6,7 @@ describe GabbyPrompt do
   it "has a nice arrow" do
     expect(subject.arrow).to end_with(">")
   end
+
   it "shows you what directory you are in" do
      Dir.mktmpdir do | dir | 
        Dir.chdir(dir) do 
@@ -13,9 +14,30 @@ describe GabbyPrompt do
          expect(subject.directory_position).to eq(realpath)
        end
     end
-    
   end
-  it "shows you the git branch"
+
+  it "shows you the git branch" do
+   Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        # Initialize a Git repository
+        `git init`
+
+        # Create a test file
+        File.open("test.txt", "w") { |file| file.write("This is a test file.") }
+
+        # Add and commit the test file
+        `git add .`
+        `git commit -m "Initial commit"`
+
+        # Switch to a new branch
+        `git checkout -b test-branch`
+
+        # Expect the GabbyPrompt object to display the correct Git branch
+        expect(subject.git_branch).to eq("test-branch")
+      end
+   end
+  end
+
   it "renders the whole prompt" do
     expect(subject.prompt).to end_with(">")
   end
